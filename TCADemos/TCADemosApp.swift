289f4a02a37d65e06370d5d5a2f7f9d143fa9f23
Andy Zheng <360475097@qq.com>
2024-12-30 22:10:07 +0800

    Initial Commit

---
+++ TCADemos/TCADemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADemosApp.swift
+//  TCADemos
+//
+//  Created by 郑敏 on 2024/12/30.
+//
+
+import SwiftUI
+
+@main
+struct TCADemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCADemosTests/TCADemosTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCADemosTests.swift
+//  TCADemosTests
+//
+//  Created by 郑敏 on 2024/12/30.
+//
+
+import Testing
+@testable import TCADemos
+
+struct TCADemosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
